9d62110d093040a1d871d661d171a1d59c4aa6a8
thryec <thryec@gmail.com>
2022-06-26 21:37:11 +0800

    added remappings file for dependencies

--- src/Patreon.sol
+++ src/Patreon.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/console2.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "forge-std/console2.sol";
 
 contract Patreon is ReentrancyGuard {
     //------------------- Variables ------------------- //
