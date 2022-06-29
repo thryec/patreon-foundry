d617166bf0e78e923f17d658f2e45645b7d134bb
thryec <thryec@gmail.com>
2022-06-29 22:20:22 +0800

    added merkle whitelist for creator and sample script to create merkle tree

---
+++ src/CreatorList.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract CreatorWhitelist is Ownable {
+    // public mint
+    bytes32 public creatorListMerkleRoot;
+
+    // Frontend verify functions
+    function verifyMsgSender(address userAddress, bytes32[] memory proof)
+        public
+        view
+        returns (bool)
+    {
+        return _verify(proof, _hash(userAddress), creatorListMerkleRoot);
+    }
+
+    // Internal verify functions
+    function _verifyMsgSender(bytes32[] memory proof)
+        internal
+        view
+        returns (bool)
+    {
+        return _verify(proof, _hash(msg.sender), creatorListMerkleRoot);
+    }
+
+    function _verify(
+        bytes32[] memory proof,
+        bytes32 addressHash,
+        bytes32 whitelistMerkleRoot
+    ) internal pure returns (bool) {
+        return MerkleProof.verify(proof, whitelistMerkleRoot, addressHash);
+    }
+
+    function _hash(address _address) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_address));
+    }
+
+    function setcreatorListMerkleRoot(bytes32 merkleRoot) external onlyOwner {
+        creatorListMerkleRoot = merkleRoot;
+    }
+
+    modifier onlyCreatorList(bytes32[] memory proof) {
+        require(
+            _verifyPublicSender(proof),
+            "CreatorWhitelist: Caller is not whitelisted"
+        );
+        _;
+    }
+}
