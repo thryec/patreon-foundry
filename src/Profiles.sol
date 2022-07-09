8e921159f77af2195650750cac062c8fee1b759e
thryec <thryec@gmail.com>
2022-07-09 11:39:37 +0800

    added profile storage tests

--- src/Profiles.sol
+++ src/Profiles.sol
@@ -4,25 +4,29 @@ pragma solidity ^0.8.13;
 import {console} from "forge-std/console.sol";
 
 contract Profiles {
-    mapping(address => bytes32) public profiles; // maps user addresses to IPFS hash containing user data
+    mapping(address => string) public profiles; // maps user addresses to IPFS hash containing user data
     address[] public addressList; // stores address of all profiles
 
-    function addProfile(address user, bytes32 ipfsHash) public {
+    function addProfile(address user, string calldata ipfsHash) public {
         profiles[user] = ipfsHash;
         addressList.push(user);
     }
 
-    function updateProfile(address user, bytes32 updatedHash) public {
-        require(msg.sender == user);
+    function updateProfile(address user, string calldata updatedHash) public {
+        require(msg.sender == user, "updating requires sender to be owner");
         profiles[user] = updatedHash;
     }
 
     function deleteProfile(address user) public {
-        require(msg.sender == user);
+        require(msg.sender == user, "deleting requires sender to be owner");
         delete profiles[user];
     }
 
-    function getProfile(address user) public view returns (bytes32 ipfsHash) {
+    function getProfile(address user)
+        public
+        view
+        returns (string memory ipfsHash)
+    {
         return profiles[user];
     }
 
--- test/Profiles.t.sol
+++ test/Profiles.t.sol
@@ -9,11 +9,46 @@ import {console} from "forge-std/console.sol";
 contract ProfileTest is Test {
     Profiles internal profile;
 
+    address alice = address(0x1);
+    address bob = address(0x2);
+
+    string testLink1 =
+        "https://ipfs.infura.io/ipfs/bafybohew2j2wbn3mzl7dakkoklstoas4jq3rj7wgiv6mmtvk7v7a";
+    string testLink2 =
+        "https://ipfs.infura.io/ipfs/bafybohc3gokgtwtxnfwgalh7qftpipwrjlk7lxifwfgrajnvk52q";
+    string emptyString = "";
+
     function setUp() public {
         profile = new Profiles();
     }
 
-    function testAddingProfile() public {}
+    function testAddProfile() public {
+        profile.addProfile(alice, testLink1);
+        string memory aliceProfile = profile.getProfile(alice);
+        assertEq(aliceProfile, testLink1);
+    }
 
-    function testGettingProfile() public {}
+    function testUpdateProfile() public {
+        vm.prank(alice);
+        profile.updateProfile(alice, testLink2);
+        string memory aliceProfile = profile.getProfile(alice);
+        assertEq(aliceProfile, testLink2);
+    }
+
+    function testUpdateProfileRequiresOwner() public {
+        vm.expectRevert(bytes("updating requires sender to be owner"));
+        profile.updateProfile(alice, testLink2);
+    }
+
+    function testDeleteProfile() public {
+        vm.prank(alice);
+        profile.deleteProfile(alice);
+        string memory aliceProfile = profile.getProfile(alice);
+        assertEq(aliceProfile, emptyString);
+    }
+
+    function testDeleteProfileRequiresOwner() public {
+        vm.expectRevert(bytes("deleting requires sender to be owner"));
+        profile.deleteProfile(alice);
+    }
 }
